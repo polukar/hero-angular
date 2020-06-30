8550e3fdad7afa225c8aa7d567a0ddafbae80c20
plukarov <ap@finfort.ru>
2020-06-30 11:27:42 +0300

    hero

---
+++ src/app/heroes/mock.heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 0, name: 'Dr Nice' },
+  { id: 1, name: 'Narco' },
+  { id: 2, name: 'Bombasto' },
+  { id: 3, name: 'Celeritas' },
+  { id: 4, name: 'Magneta' },
+  { id: 5, name: 'RubberMan' },
+  { id: 6, name: 'Dynama' },
+  { id: 7, name: 'Dr IQ' },
+  { id: 8, name: 'Magma' }
+];
